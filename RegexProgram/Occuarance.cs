4b935230fdec54cc76d0bfec5c004e99c6f9dee1
puneethcm <puneethkumarcm8050@gmail.com>
2023-04-17 14:45:07 +0530

    find occurance' git push origin FindOccurance

---
+++ RegexProgram/Occuarance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexProgram
+{
+	public class Occuarance
+	{
+		public static void FindOccuarance()
+		{
+			string pattren = "fox(es)?";
+            string s = @"foxes are omnivorous mammals belonging to several genera of the family Canidae fox.";
+            Regex regex = new Regex(pattren);
+            MatchCollection matchs = regex.Matches(s);
+
+			Console.WriteLine("fox(es) occured " + matchs.Count+" times ");
+        }
+	}
+}
+
--- RegexProgram/Program.cs
+++ RegexProgram/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     {
         Console.WriteLine("Regex Program");
         Console.WriteLine("Choose any one option");
-        Console.WriteLine("\n1:Simple demo\n2:Lower case with underscore\n3:Find HTML tags");
+        Console.WriteLine("\n1:Simple demo\n2:Lower case with underscore\n3:Find HTML tags\n4:Find Occurance");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
@@ -19,6 +19,9 @@ internal class Program
             case 3:
                 string input = "<p> The <code> Regex </code> is a compiled\nrepresentation of a regular expression. </p>";
                 HTMLTag.FindHTMLTag(input);
+                break;
+            case 4:
+                Occuarance.FindOccuarance();
                 break;
         }
         Console.ReadLine();
